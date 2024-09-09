cef50398add2dedb050e9482f84ef5a659c51f36
Freddy Ayala <freddyayala@microsoft.com>
2024-09-09 19:23:45 +0200

    promptcorrection

--- foundation/standalone/app/backend/FlaskApp/approaches/retrievethenread.py
+++ foundation/standalone/app/backend/FlaskApp/approaches/retrievethenread.py
@@ -11,34 +11,11 @@ from .approach import Approach
 # (answer) with that prompt.
 class RetrieveThenReadApproach(Approach):
 
-    template = \
-"You are an intelligent assistant helping users with their questions. " + \
-"Use 'you' to refer to the individual asking the questions even if they ask with 'I'. " + \
-"Answer the following question using only the data provided in the sources below. " + \
-"For tabular information return it as an html table. Do not return markdown format. "  + \
-"Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. " + \
-"If you cannot answer using the sources below, say you don't know. " + \
-"""
-
-###
-Question: 'What is the deductible for the employee plan for a visit to Overlake in Bellevue?'
-
-Sources:
-info1.txt: deductibles depend on whether you are in-network or out-of-network. In-network deductibles are $500 for employee and $1000 for family. Out-of-network deductibles are $1000 for employee and $2000 for family.
-info2.pdf: Overlake is in-network for the employee plan.
-info3.pdf: Overlake is the name of the area that includes a park and ride near Bellevue.
-info4.pdf: In-network institutions include Overlake, Swedish and others in the region
-
-Answer:
-In-network deductibles are $500 for employee and $1000 for family [info1.txt] and Overlake is in-network for the employee plan [info2.pdf][info4.pdf].
-
-###
-Question: '{q}'?
-
-Sources:
-{retrieved}
-
-Answer:
+    template = """
+<|im_start|>system
+You are a virtual assistant. Be brief in your answers.
+For tabular information return it as an html table. Do not return markdown format.
+<|im_end|>
 """
 
     def __init__(self, search_client: SearchClient, openai_deployment: str, sourcepage_field: str, content_field: str):
